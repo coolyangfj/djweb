8a63e69181d0a22ed780ea9db7a9b7b58cd02fb3
coolyang <384762270@qq.com>
2018-10-21 10:35:40 +0800

    Initial commit

---
+++ apps/users/adminx.py
@@ -0,0 +1,24 @@
+# users/adminx.py
+
+import xadmin
+
+from .models import EmailVerifyRecord,Banner
+
+#xadmin中这里是继承object，不再是继承admin
+class EmailVerifyRecordAdmin(object):
+    # 显示的列
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    # 搜索的字段，不要添加时间搜索
+    search_fields = ['code', 'email', 'send_type']
+    # 过滤
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url','index', 'add_time']
+    search_fields = ['title', 'image', 'url','index']
+    list_filter = ['title', 'image', 'url','index', 'add_time']
+
+
+xadmin.site.register(EmailVerifyRecord,EmailVerifyRecordAdmin)
+xadmin.site.register(Banner,BannerAdmin)
